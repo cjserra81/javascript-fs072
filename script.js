87894c1e53f5bdf285b96cad39be63b6aad1e8c1
unknown <josejunior0019@digitalcollege.internal>
2025-09-16 09:40:31 -0300

    trabalhando com dados primitivos

---
+++ script.js
@@ -0,0 +1,10 @@
+ 
+ // trabalhando com saídas de dados.
+ console.log("fui criado no arquivo JavaScrip.");
+
+ //tipos de dados.
+ console.log("Hello!");  //string
+ console.log(29.5);  //number
+ console.log(true, false);  //boolean
+ console.log(null);  // null(nulo)
+ console.log(undefined);  // undefined
