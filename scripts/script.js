89a0a8c18357c73e3dba92a1388fb48707bd21f0
unknown <josejunior0019@digitalcollege.internal>
2025-09-23 12:07:58 -0300

    java script

---
+++ scripts/script.js
@@ -0,0 +1,103 @@
+// estrutura de controle de fluxo
+
+// const age = prompt("digite sua idade:");
+
+// if(age >= 18) {
+//     if (age >= 65) {
+//         alert("você está na melhor idade.");
+//     } else {
+//         alert("você é maior de idade.");
+//     }
+// } else {
+//     alert("você é menor de idade.");
+// } 
+
+// if (age >= 65){
+//     alert ("você está na melhor idade.");
+// }else if ( age >= 18){
+//     alert("você é maior de idade.");
+// }else {
+//     alert ("você é menor de idade.");
+// };
+
+// const temperatura = Number(prompt ("temperatura:"));
+
+// if ( temperatura > 35){
+//     alert ("calor extremo.");
+// }else if ( temperatura >=25 ){
+//     alert("clima quente.");
+// }else if( temperatura >=15 ){
+//     alert("clima agradável.");
+// }else if (temperatura >=5 ){
+//     alert("clima frio.");
+// }else {
+//     alert("frio intenso.");
+    
+// }
+
+// const color = "yellow";
+
+// switch(color){
+//     case "green":
+//         alert ("Go!");
+//         break;
+//     case "yellow":
+//         alert("Atention!");
+//         break
+//     case "red":
+//         alert ("Stop!");
+//         break;
+//     default:
+//         alert( "Invalid color!")
+
+// }
+
+
+// ESTRUTURAS DE REPETIÇÃO
+
+// FOR = repete enquanto for verdadeira
+
+// WHILE 
+
+// inicializador;condição;incremento/decremento
+
+
+
+// for(let i = 10; i >= 0; i--){
+//    console.log(i);
+// }
+
+
+// let i = 0;
+
+// for( ;i <= 10;){
+//     console.log(i);
+//     i++;
+// };
+
+
+// let i = 0;
+
+// while( i <= 10){
+//     console.log(i);
+//     i++;
+// };
+
+// let i =0;
+
+// while( i <= 10){
+//     if (i % 2 === 0){
+//         console.log(i);
+//     };
+//     i++;
+// };
+
+const palavra = prompt ("digite uma palavra:");
+
+for(let i = 1; i <=5; i++){
+    console.log(palavra);
+};
+        
+    
+
+
