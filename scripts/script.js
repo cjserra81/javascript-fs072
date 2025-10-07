20e9d2fcd98bb9565fcf30f8c15432863fe17827
unknown <josejunior0019@digitalcollege.internal>
2025-10-07 08:33:53 -0300

    javascript

--- scripts/script.js
+++ scripts/script.js
@@ -92,12 +92,147 @@
 //     i++;
 // };
 
-const palavra = prompt ("digite uma palavra:");
+// const palavra = prompt ("digite uma palavra:");
 
-for(let i = 1; i <=5; i++){
-    console.log(palavra);
-};
+// for(let i = 1; i <=5; i++){
+//     console.log(palavra);
+// };
         
     
+//aula do dia 30
+
+// const numero1 = Number (prompt("digite o primeiro numero:"));
+// const numero2 = Number (prompt ("digite o segundo numero:"));
+
+// if(numero1 > numero2){
+//     alert(numero1 + "é maior");
+
+// }else{
+//     alert(numero2 + "é maior");
+// }
+
+// const numero = Number(prompt("digite um numero"));
+
+// if(numero > 0){
+//     alert("positivo");
+// }else if(numero ===0 ){
+//     alert("numero neutro")
+// }
+// else{
+//     alert("negativo");
+// }
 
 
+// const letra = prompt ("digite uma letra");
+
+// if(letra === "f" || letra === "F"){
+//     alert("feminino" );
+
+// }
+// else if(letra === "m" || letra === "M"){
+//     alert("masculino");
+// }
+// else{
+//     alert("digite a letra que eu pedi, desgraça!")
+// }
+
+// const letra = prompt("Digite uma letra:");
+
+// switch(letra){
+//     case "M":
+//         alert("MAsculino");
+//         break;
+//     case "F":
+//         alert("Feminino");
+//         break;
+//     case "m":
+//         alert("MAsculino");
+//         break;
+//     case "f":
+//         alert("Feminino");
+//         break;
+//     default:
+//         alert("Digite uma letra válida.")
+// }
+
+
+// //tipos de dados primitivos
+// String== texto
+// Number==numeros
+// Boolean==true or False
+// null==ausencia de valor
+// undefined==
+
+// tipos não primitivos
+// Array==lista(que podem ser mistas)
+// Object==
+// function==
+
+//ARRAY
+
+// const lista = [
+//     [1,2,3],
+//     ["alex " , "Levi" , "Jairison"],
+//     [true , false]
+// ];
+
+// console.log(lista[1][1]);
+
+//OBJECT LITERAL chave e valor são as proppriedades do objeto
+
+// const pessoa = {
+//     nome:"Alex",
+//     idade:  29,
+//     altura: 1.75
+// }
+
+// console.log(pessoa.altura);
+
+// const carro = {
+//     marca: "fiat",
+//     modelo: "uno",
+//     ano: 1997
+// }
+
+// console.log(carro.ano);
+
+
+// const lista = [
+//     ["uva","maçã"],
+//     [
+//         {
+//             nome:"alex",
+//             idade: 29
+//         },
+//         {
+//             nome:"josé",
+//             idade: 46
+//         },
+//         {
+//             nome:"freitas",
+//             idade: 56
+//         }
+//     ],
+//     [0,1,2,3]
+// ];
+
+
+
+// console.log(lista[1] [2].nome,idade);
+
+const nomes = ["alex","levi","hebston","paulo","yasmin","marcos","wesley","aubameyang"];
+
+for(let index = 0; index < nomes.length; index++){
+    if(nomes[index]=== "hebston"){
+        console.log(nome[index] + "é mala!");
+    }
+
+}
+for(let index = 0; index < nomes.length; index++){
+    console.log(nome[index]);
+}
+
+// percorra a lista de numeros
+// se o numero for divisivel por 2; imprima no console  o numero + mmar;
+// se o numero for divisivel por 5; imprima no console o numero + menino;
+// se o numero for divisivel por 2 e por 5; imprima no console o numero + marmenino;git
